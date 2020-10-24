f20e4130b3760e33bdb9036d1d2ebf582b134455
ezwade <65866574+ezwade@users.noreply.github.com>
2020-10-24 14:27:22 -0400

    Add files via upload

---
+++ StarterCode/assets/js/app.js
@@ -0,0 +1,48 @@
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#scatter")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("assets/data/data.csv", function(data) {
+
+
+  console.log(data.poverty) 
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([0, 30])
+    .range([ 0, width ]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 30])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Add dots
+  svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function(d) {return x(d.healthcare);})
+      .attr("cy", function(d) {return y(d.poverty);})
+      .attr("r", 1.5)
+      .style("fill", function (d) {
+        return d.abbr;})
+
+
+})
